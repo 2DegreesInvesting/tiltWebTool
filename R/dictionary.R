05968174f895a192da2a568b2ba02ec6bdcd7155
Mauro Lepore <maurolepore@gmail.com>
2024-05-28 23:05:23 +0000

    wip

--- R/dictionary.R
+++ R/dictionary.R
@@ -1,4 +1,7 @@
-#' The URL where we collaboratively define columns
+#' Dictionary
+#'
+#' Edit at: `r dic_edit_url()`.
+#' Published at: `r dic_publish_url()`.
 #'
 #' @return Character
 #' @export
@@ -11,7 +14,7 @@ dictionary <- function() {
     # `colClasses` makes it do less work (so faster)
     # https://adv-r.hadley.nz/perf-improve.html#be-lazy
     read.table(
-      dic_url(),
+      dic_publish_url(),
       header = TRUE,
       colClasses = "character",
       sep = "\t"
@@ -19,9 +22,10 @@ dictionary <- function() {
   )
 }
 
-# Edit
-# https://docs.google.com/spreadsheets/d/1gOZRS9_0yUgR7UXgsf4WmDoAEXfUQy3Mz0MZM1Xvv40/edit#gid=105958234
-dic_url <- function() {
-  # Published here with `File > Share > Publish to web`
+dic_edit_url <- function() {
+  "https://docs.google.com/spreadsheets/d/1gOZRS9_0yUgR7UXgsf4WmDoAEXfUQy3Mz0MZM1Xvv40/edit#gid=105958234"
+}
+dic_publish_url <- function() {
+  # File > Share > Publish to web
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vSpeL37_DinYYKaQNKWdjL0oKU_VrEKMDBN1dBfBpO6AgSyHLTt8WiiXFojcbPclsPTE0VliDWvRKxh/pub?gid=105958234&single=true&output=tsv"
 }
