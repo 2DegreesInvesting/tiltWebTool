9b7236504d8ac685db35df97ef3b4c57f7e93b54
Mauro Lepore <maurolepore@gmail.com>
2024-07-04 00:02:09 +0000

    Link to download

--- R/side_bar.R
+++ R/side_bar.R
@@ -3,6 +3,16 @@ side_bar <- function() {
     selectChoices("indicator"),
     selectChoices("level"),
     selectChoices("weight", "(product level)"),
-    downloadButton("download", "Download .tsv")
+    download()
   )
+}
+
+
+download <- function() {
+  tags$p(
+    tags$a("Download", href = "https://zenodo.org/records/12638774"),
+    tags$a("under"),
+    tags$a("license CC BY-NC-ND 4.0", href = "https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode")
+  )
+
 }
