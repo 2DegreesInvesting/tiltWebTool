edb533624d2dfbf851e6f5118661deea04b5a74c
Mauro Lepore <maurolepore@gmail.com>
2024-07-04 00:15:47 +0000

    The download button now links to zenodo

--- R/side_bar.R
+++ R/side_bar.R
@@ -1,18 +1,26 @@
 side_bar <- function() {
   sidebar(
-    selectChoices("indicator"),
-    selectChoices("level"),
-    selectChoices("weight", "(product level)"),
-    download()
+    card(
+      selectChoices("indicator"),
+      selectChoices("level"),
+      selectChoices("weight", "(product level)")
+    ),
+    card(licensed_download())
   )
 }
 
+licensed_download <- function() {
+  tags$p(
+    download(),
+    "(license: ", license(), ")"
+  )
+}
 
 download <- function() {
-  tags$p(
-    tags$a("Download", href = "https://zenodo.org/records/12638774"),
-    tags$a("under"),
-    tags$a("license CC BY-NC-ND 4.0", href = "https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode")
-  )
-
+  tags$a("Download", href = "https://zenodo.org/records/12638774")
 }
+
+license <- function() {
+  tags$a("CC BY-NC-ND 4.0", href = "https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode")
+}
+
