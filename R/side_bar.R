d1b0aa74e087d46519764a6cfa3be5305f0c0d10
Mauro Lepore <maurolepore@gmail.com>
2024-07-04 00:18:23 +0000

    Refactor

--- R/side_bar.R
+++ R/side_bar.R
@@ -8,19 +8,3 @@ side_bar <- function() {
     card(licensed_download())
   )
 }
-
-licensed_download <- function() {
-  tags$p(
-    download(),
-    "(license: ", license(), ")"
-  )
-}
-
-download <- function() {
-  tags$a("Download", href = "https://zenodo.org/records/12638774")
-}
-
-license <- function() {
-  tags$a("CC BY-NC-ND 4.0", href = "https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode")
-}
-
--- R/utils-download.R
+++ R/utils-download.R
@@ -1,4 +1,14 @@
-time_stamp <- function(x) {
-  stamp <- format(Sys.time(), "%Y-%m-%e_%H:%M:%S_")
-  paste0(stamp, x)
+licensed_download <- function() {
+  tags$p(
+    download(),
+    "(license: ", license(), ")"
+  )
+}
+
+download <- function() {
+  tags$a("Download", href = "https://zenodo.org/records/12638774")
+}
+
+license <- function() {
+  tags$a("CC BY-NC-ND 4.0", href = "https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode")
 }
