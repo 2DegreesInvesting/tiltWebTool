2ee38df2543ee5fe7578cef18d4b16680a51513c
Mauro Lepore <maurolepore@gmail.com>
2024-07-12 00:08:57 +0000

    wip

--- R/test_app.R
+++ R/test_app.R
@@ -2,12 +2,12 @@ test_app <- function() {
   from <- function(parent) {
     sprintf("read_parquet('%s/**/*.parquet', hive_partitioning = true)", parent)
   }
-  company_data <- function(parent) {
+  connect_tbl <- function(parent) {
     con <- DBI::dbConnect(duckdb::duckdb(), dbdir = ":memory:")
     dplyr::tbl(con, from = from(parent))
   }
 
-  company_data <- company_data("/pushymatador/fake/company")
+  company_data <- connect_tbl("/pushymatador/fake/company")
 
   ui <- fluidPage(
     numericInput(".n", "Rows to show", min = 1, max = 10, value = 5),
