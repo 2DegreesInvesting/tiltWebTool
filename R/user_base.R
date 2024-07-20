36be6c4dcdb4250754acef0c9b37ad12f845352d
Mauro Lepore <maurolepore@gmail.com>
2024-07-20 01:02:34 +0000

    Polish

--- R/user_base.R
+++ R/user_base.R
@@ -1,8 +1,8 @@
 user_base <- function() {
   data.frame(
     # FIXME: The app shows these values no matter what
-    user = config::get("user_name", file = config_path("private.txt")),
-    pass = config::get("password", file = config_path("private.txt")),
+    user = config::get("user_name", file = config_path("credentials.txt")),
+    pass = config::get("password", file = config_path("credentials.txt")),
     stringsAsFactors = FALSE
   )
 }
