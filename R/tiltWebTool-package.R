5a6b22caec31d3292e711a52b040bf63ea65d974
Mauro Lepore <maurolepore@gmail.com>
2024-07-24 15:23:02 -0600

    * The data is now a parquet dataset (#145)
    * Use arrow db/
    
    * Use db/
    
    * Prune
    
    * Refactor
    
    * Refactor
    
    * Refactor
    
    * Refactor
    
    * Refactor

--- R/tiltWebTool-package.R
+++ R/tiltWebTool-package.R
@@ -6,25 +6,10 @@
 #' @import shiny
 #' @importFrom dplyr .data
 #' @importFrom dplyr filter
-#' @importFrom dplyr group_by
-#' @importFrom dplyr mutate
-#' @importFrom dplyr n
 #' @importFrom dplyr select
-#' @importFrom dplyr summarise
-#' @importFrom dplyr summarize
-#' @importFrom dplyr ungroup
-#' @importFrom tidyselect all_of
-#' @importFrom tidyselect everything
+#' @importFrom pkgload load_all
 #' @importFrom tidyselect matches
+#' @importFrom utils head
 #' @importFrom utils packageVersion
-#' @importFrom utils read.table
 ## usethis namespace: end
 NULL
-
-#' @export
-#' @importFrom tiltIndicator unnest_product
-tiltIndicator::unnest_product
-
-#' @export
-#' @importFrom tiltIndicator unnest_company
-tiltIndicator::unnest_company
