1052e4140232079a6131fc7ddc092a80d2747aa4
Mauro Lepore <maurolepore@gmail.com>
2024-07-24 15:54:35 -0600

    * Remove the weight selector (#146)

--- tests/testthat/test-utils-choices.R
+++
@@ -1,5 +0,0 @@
-test_that("weight choices hasn't changed", {
-  expect_snapshot(unselected_choices("equal_weight"))
-  expect_snapshot(unselected_choices("best_case"))
-  expect_snapshot(unselected_choices("worst_case"))
-})
