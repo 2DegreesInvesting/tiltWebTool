add7151e3844b184adabf1f9f8921d898b879340
Mauro Lepore <maurolepore@gmail.com>
2024-08-08 21:26:45 +0000

    Use latest dictionary

--- R/dictionary.R
+++ R/dictionary.R
@@ -1,18 +1,7 @@
 dictionary <- function() {
-  arrow::read_csv_arrow(dictionary_url("csv"))
+  arrow::read_tsv_arrow(get_dictionary("url"))
 }
 
-dictionary_url <- function(type = c("web", "csv")) {
-  type <- match.arg(type)
-
-  switch(type,
-    web = web_url(),
-    csv = paste0(web_url(), "&output=", type),
-    stop("Unknown type: ", type)
-  )
-}
-
-web_url <- function() {
-  # File > Share > Publish to web
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSpeL37_DinYYKaQNKWdjL0oKU_VrEKMDBN1dBfBpO6AgSyHLTt8WiiXFojcbPclsPTE0VliDWvRKxh/pub?gid=105958234&single=true"
+get_dictionary <- function(x) {
+  config::get(x, file = config_path("dictionary"))
 }
