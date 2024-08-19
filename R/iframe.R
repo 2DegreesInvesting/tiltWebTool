755b70bcfc599eb6128bc2cfd186384873c171eb
Mauro Lepore <maurolepore@gmail.com>
2024-08-19 17:39:45 +0000

    Move iframe_card() to R/iframe

---
+++ R/iframe.R
@@ -0,0 +1,17 @@
+iframe_card <- function(section) {
+  x <- config::get(section, file = config_path("iframe"))
+
+  header <- card_header(markdown(x$header))
+  body <- iframe <- shiny::tags$iframe(
+    width = x$width,
+    height = x$height,
+    src = x$src,
+    title = "YouTube video player",
+    frameborder = "0",
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",
+    referrerpolicy = "strict-origin-when-cross-origin",
+    allowfullscreen = TRUE
+  )
+
+  card(header, body)
+}
