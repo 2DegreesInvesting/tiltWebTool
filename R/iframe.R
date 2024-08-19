894ff0dba6da9c49d6a40fafa0d9eaa2b9e638e5
Mauro Lepore <maurolepore@gmail.com>
2024-08-19 18:18:54 +0000

    Rearrange cards

--- R/iframe.R
+++ R/iframe.R
@@ -15,3 +15,23 @@ iframe_card <- function(section) {
 
   card(header, body)
 }
+
+iframe_section <- function(section) {
+  x <- config::get(section, file = config_path("iframe"))
+
+  body <- tags$iframe(
+    width = x$width,
+    height = x$height,
+    src = x$src,
+    title = "YouTube video player",
+    frameborder = "0",
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",
+    referrerpolicy = "strict-origin-when-cross-origin",
+    allowfullscreen = TRUE
+  )
+
+  list(
+    header = x$header,
+    body = body
+  )
+}
