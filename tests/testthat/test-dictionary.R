dd6e21d0049ae7f29e1a884acc746262aa198378
Mauro Lepore <maurolepore@gmail.com>
2024-08-24 23:14:40 +0000

    Avoid curl

--- tests/testthat/test-dictionary.R
+++ tests/testthat/test-dictionary.R
@@ -1,9 +1,7 @@
 test_that("yields a dataframe", {
-  skip_if_offline()
   expect_s3_class(dictionary(), "data.frame")
 })
 
 test_that("get_dictionary() yields the same URL", {
-  skip_if_offline()
   expect_snapshot(get_dictionary("url"))
 })
