8a8124155a59291aa5d8dd8df5409f57afc98477
Kalash Singhal <kalash@2degrees-investing.org>
2024-08-30 10:15:49 +0200

    revert change

--- tests/testthat/test-iframe.R
+++ tests/testthat/test-iframe.R
@@ -2,6 +2,6 @@ test_that("fails gracefully", {
   expect_snapshot(error = TRUE, iframe())
 })
 
-# test_that("returns a bslib_fragment", {
-#   expect_s3_class(iframe(welcome_id()), "shiny.tag")
-# })
+test_that("returns a bslib_fragment", {
+  expect_s3_class(iframe(welcome_id()), "shiny.tag")
+})
